2f572b91f030b76192e712fca19c4f34b9def5f3
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-30 01:46:38 +0600

    Recursive solution added

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -13,3 +13,16 @@ class Solution:
                 low = mid + 1
         return -1
 # Recursive Way
+class Solution:
+    def binarySearch(self, nums, left, right, target):
+        if left > right:
+            return -1
+        mid = (left + right)//2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            return self.binarySearch(nums, left, mid-1, target)
+        else:
+            return self.binarySearch(nums, mid+1, right, target)
+    def search(self, nums: List[int], target: int) -> int:
+        return self.binarySearch(nums, 0, len(nums)-1, target)
