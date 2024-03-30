86ed8f70daf0640ac80f074633cc259c4e38eb0b
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-30 17:40:26 +0600

    gfg  floor solution added

---  Implement Lower Bound.py
+++  Implement Lower Bound.py
@@ -11,3 +11,20 @@ def lowerBound(arr: [int], x: int, n: int) -> int:
         else:
             low = mid + 1
     return floor
+#GFG Solution
+class Solution:
+    #User function Template for python3
+    
+    #Complete this function
+    def findFloor(self,A,N,X):
+        low, high = 0, N - 1
+        floor_index = -1
+        while low <= high:
+            mid = (low + high) // 2
+            if A[mid] <= X:
+                floor_index = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        return floor_index
+    
