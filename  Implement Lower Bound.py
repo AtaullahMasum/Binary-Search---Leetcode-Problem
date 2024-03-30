00a83e029dc7864e50e004900f2741586a678c8c
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-30 13:48:13 +0600

    lower bound

---
+++  Implement Lower Bound.py
@@ -0,0 +1,13 @@
+def lowerBound(arr: [int], x: int, n: int) -> int:
+    # Write your code here.
+    floor = n
+    low = 0
+    high = n-1
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] >= x:
+            floor = mid
+            high = mid -1 
+        else:
+            low = mid + 1
+    return floor
