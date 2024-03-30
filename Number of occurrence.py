d57787000d8e713299ed4962c5fef350d52ba58e
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-31 00:26:53 +0600

    number of occurrence an element

---
+++ Number of occurrence.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+class Solution:
+        def lowerBound(self, arr, n, x):
+            low , high = 0,  n-1
+            lb = n
+            while low <= high:
+                mid =(low+high)//2
+                if arr[mid] >= x:
+                    lb = mid
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return lb
+        def upperBound(self, arr, n, x):
+            low, high = 0, n-1
+            ub = n
+            while low <= high:
+                mid = (low + high)//2
+                if arr[mid] > x:
+                    ub = mid
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return ub-1
+        def count(self,arr, n, x):
+            # code here
+            lb = self.lowerBound(arr, n, x)
+            if lb == n or arr[lb] != x:
+                return 0
+            ub = self.upperBound(arr, n, x)
+            return ub - lb + 1
