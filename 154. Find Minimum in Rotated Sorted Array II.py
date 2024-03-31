fa2640a6a6f50a8085ab4874fabec5b10bec0226
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-31 19:59:34 +0600

    search space already sorted then nums[left] is ans

--- 154. Find Minimum in Rotated Sorted Array II.py
+++ 154. Find Minimum in Rotated Sorted Array II.py
@@ -34,3 +34,28 @@ class Solution:
                 ans = min(ans, nums[mid])
                 right = mid - 1
         return ans
+# Another solution added
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        ans = float('inf')
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left+right)//2
+            if nums[left] == nums[mid] == nums[right]:
+                ans = min(ans, nums[left])
+                left = left + 1
+                right = right -1
+                continue
+            # if search space already sorted 
+            # then nums[left ] is always smaller in that search space
+            if nums[left] < nums[right]:
+                ans = min(ans, nums[left])
+                break
+            if nums[left]<= nums[mid]:
+                ans = min(ans, nums[left])
+                left = mid + 1
+            else:
+                ans = min(ans, nums[mid])
+                right = mid - 1
+        return ans
+
