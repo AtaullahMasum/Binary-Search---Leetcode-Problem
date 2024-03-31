a31873311f88226a0930489be4784891e29192ea
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-31 13:59:36 +0600

    without using elif

--- 154. Find Minimum in Rotated Sorted Array II.py
+++ 154. Find Minimum in Rotated Sorted Array II.py
@@ -15,3 +15,22 @@ class Solution:
                 ans = min(ans, nums[mid])
                 right = mid - 1
         return ans
+# Without using elif 
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        ans = float('inf')
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left+right)//2
+            if nums[left] == nums[mid] == nums[right]:
+                ans = min(ans, nums[left])
+                left = left + 1
+                right = right -1
+                continue
+            if nums[left]<= nums[mid]:
+                ans = min(ans, nums[left])
+                left = mid + 1
+            else:
+                ans = min(ans, nums[mid])
+                right = mid - 1
+        return ans
