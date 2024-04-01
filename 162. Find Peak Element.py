d3f32f9b1eedc0d6cef947632e2849a0e45a20dd
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-01 12:49:19 +0600

    brute force

--- 162. Find Peak Element.py
+++ 162. Find Peak Element.py
@@ -1,14 +1,8 @@
 #Brute force Approch
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
        
         for i in range(len(nums)):
-            if i == 0 and float('-inf') < nums[i] > nums[i+1]:
-                return i
-            if i == len(nums)-1 and nums[i-1]<nums[i] > float('-inf'):
-                return i
-            if nums[i-1]< nums[i] > nums[i+1]:
-                return i
+           if (i == 0 or nums[i-1]<nums[i] ) and (i ==len(nums)-1 or nums[i]>nums[i+1]):
+            return i
         
