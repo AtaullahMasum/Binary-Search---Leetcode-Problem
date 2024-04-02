b3d50dc2b97ea2415faac3f1d59474234be943c2
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-02 21:55:31 +0600

    optimal solution added

--- 1283. Find the Smallest Divisor Given a Threshold.py
+++ 1283. Find the Smallest Divisor Given a Threshold.py
@@ -10,3 +10,23 @@ class Solution:
             if sum <= threshold:
                 return d
         return -1
+# Using Binary Search
+# Time Complexity is O(nlongm) where n is number of element in the array and m is maximum element in these array
+class Solution:
+    def sumDivisor(self, nums, d):
+        sum = 0
+        for i in range(len(nums)):
+            sum += (nums[i]+d-1)//d #always take ceil value
+        return sum
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        low, high =1,  max(nums)
+        ans = -1
+        while low <= high:
+            mid = (low+high)//2
+            if self.sumDivisor(nums, mid) <= threshold:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
+        
