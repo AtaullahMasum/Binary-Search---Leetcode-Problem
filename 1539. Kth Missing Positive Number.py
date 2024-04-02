508a7938438955555ea0e4229f8ad305a23d7f6d
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-03 00:43:04 +0600

    optimal solution added

--- 1539. Kth Missing Positive Number.py
+++ 1539. Kth Missing Positive Number.py
@@ -8,4 +8,17 @@ class Solution:
             else:
                 break
         return k
+#Optimal Solution Added
+# Time Complexity is O(logn)
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        low , high = 0, len(arr)-1
+        while low <= high:
+            mid = (low+high)//2
+            missing = arr[mid] -(mid+1)
+            if missing < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return high + 1 + k 
         
