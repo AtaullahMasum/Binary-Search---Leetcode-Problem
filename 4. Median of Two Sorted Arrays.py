77a98f80efc07ee70c709cec1af24c731102f4d1
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-04 21:53:53 +0600

    better solution added

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -22,3 +22,48 @@ class Solution:
             return float(nums[(n+m)//2])
         else:
             return (nums[(n+m)//2-1]+nums[(n+m)//2])/2.0
+# Better Solution 
+# Time Complexity O(n+m)
+# Space Complexity O(1)
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        i, j = 0, 0
+        n, m = len(nums1), len(nums2)
+        total = (n+m)
+        index2 = total//2
+        index1 = index2 - 1
+        element1, element2 = -1,-1
+        cnt = 0
+        while i < n and j < m:
+            if nums1[i] < nums2[j]:
+                if cnt == index1:
+                    element1 = nums1[i]
+                if cnt == index2:
+                    element2 = nums1[i]
+                cnt += 1
+                i += 1
+            else:
+                if cnt == index1:
+                    element1 = nums2[j]
+                if cnt == index2:
+                    element2 = nums2[j]
+                cnt += 1
+                j += 1
+        while i < n:
+            if cnt == index1:
+                element1 = nums1[i]
+            if cnt == index2:
+                element2 = nums1[i]
+            cnt += 1
+            i += 1
+        while j < m:
+            if cnt == index1:
+                element1 = nums2[j]
+            if cnt == index2:
+                element2 = nums2[j]
+            cnt += 1
+            j += 1
+        if (n+m)%2==1:
+            return float(element2)
+        else:
+            return (element1+element2)/2.0
