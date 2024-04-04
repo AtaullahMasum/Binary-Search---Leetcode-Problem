5305fe5186094e796de16df6fd6d2df56f3f2337
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-05 00:26:37 +0600

    binary search solution added

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -67,3 +67,37 @@ class Solution:
             return float(element2)
         else:
             return (element1+element2)/2.0
+#Using Binary Search
+#Time Complexity is O(log(min(n1,n2)))
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n1 = len(nums1)
+        n2 = len(nums2)
+        if n1 > n2:
+            return self.findMedianSortedArrays(nums2, nums1)
+        low , high = 0, n1
+        left = ( n1 + n2 + 1)//2
+        n = n1 + n2
+        while low <= high:
+            mid1 = (low + high)//2
+            mid2 = left - mid1
+            l1 , r1 = float('-inf'), float('inf')
+            l2 , r2 = float('-inf'), float('inf')
+            if mid1 - 1 >= 0:
+                l1 = nums1[mid1-1]
+            if mid1 < n1:
+                r1 = nums1[mid1]
+            if mid2 - 1 >= 0:
+                l2 = nums2[mid2-1]
+            if mid2 < n2:
+                r2 = nums2[mid2]
+            if l1 <= r2 and l2 <= r1:
+                if n%2==1:
+                    return float(max(l1,l2))
+                else:
+                    return (max(l1,l2) + min(r1,r2))/2.0
+            elif l1 > r2:
+                high = mid1 - 1
+            else:
+                low = mid1 + 1
+        return 0
